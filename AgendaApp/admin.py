2ff131226789081fa36ce01944da752f4fe3075f
Deziane Sabino <deziane.sabino.santos@gmail.com>
2023-11-01 22:19:00 -0300

    inclusão cadastro de telefone

--- AgendaApp/admin.py
+++ AgendaApp/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from AgendaApp.models import Contato, Cidade
+from AgendaApp.models import Contato, Cidade, Telefone
+
+class Telefones(admin.StackedInline):
+    model = Telefone
 
 class ContatoAdmin (admin.ModelAdmin):
     list_display = [ 'id', 'nome', 'apelido', 'DataNascimento']
     list_filter = ['DataNascimento', 'cidade', 'estado']
     search_fields = ['nome', 'apelido']
+    inlines = [Telefones]
 
 
 # Register your models here.
 admin.site.register(Contato, ContatoAdmin)
 admin.site.register(Cidade)
+admin.site.register(Telefone)
