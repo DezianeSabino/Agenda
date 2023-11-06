f01d1589ec517c9b1583c3930b1db5711ed2adc0
Deziane Sabino <deziane.sabino.santos@gmail.com>
2023-11-06 20:45:09 -0300

    Inclusão  do campo Interesses

--- AgendaApp/admin.py
+++ AgendaApp/admin.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
-from AgendaApp.models import Contato, Cidade, Telefone
+from AgendaApp.models import Contato, Cidade, Telefone, Interesse
 
 class Telefones(admin.StackedInline):
     model = Telefone
+    extra = 1
 
 class ContatoAdmin (admin.ModelAdmin):
     list_display = [ 'id', 'nome', 'apelido', 'DataNascimento']
+    list_display_links = ['id', 'nome']
     list_filter = ['DataNascimento', 'cidade', 'estado']
     search_fields = ['nome', 'apelido']
     inlines = [Telefones]
+    filter_horizontal = ['interesses']
 
 
 # Register your models here.
 admin.site.register(Contato, ContatoAdmin)
 admin.site.register(Cidade)
 admin.site.register(Telefone)
+admin.site.register(Interesse)
